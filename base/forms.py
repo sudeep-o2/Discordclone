86fea57675d859918dfdb22b1a8193575c8cb9e3
Sudeep <sudeepsavai16@gmail.com>
2023-05-31 12:35:18 +0530

    deploy try

---
+++ base/forms.py
@@ -0,0 +1,21 @@
+from . models import Room,User
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.models import User
+
+class RoomForm(ModelForm):
+    class Meta:
+        model=Room
+        fields='__all__'
+        exclude=['host','participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model=User
+        fields=['username','email','name','avatar','about']
+
+
+class myUserCreationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['name','username','email','password1','password2','about']
